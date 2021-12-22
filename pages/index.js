54e037bb006467d498abc66af3712f81cdd15e06
Taylor Ellis <taylor.ellis@enlinx.com>
2021-12-22 12:56:37 -0700

    finished build as of tutorial vid

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import Center from '../components/Center';
+import Player from '../components/Player';
 import Sidebar from '../components/Sidebar';
 
 export default function Home() {
@@ -10,7 +11,9 @@ export default function Home() {
 				<Sidebar />
 				<Center />
 			</main>
-			<div>{/* Player */}</div>
+			<div className='sticky bottom-0'>
+				<Player />
+			</div>
 		</div>
 	);
 }
